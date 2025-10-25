bfc4de229d4fa1c2a568294fef95f053a947b5b2
Mikael Kohlmyr <mkohlmyr@Mikaels-Air.lan>
2025-10-25 09:24:28 +0100

    chore: basic templating, stylesheet reset

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,24 @@
+import markdownIt from "markdown-it";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+
+
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  eleventyConfig.setLibrary("md", markdownIt(markdownItOptions));
+
   return {
     dir: {
       input: "src",
       output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
     }
   }
 }
